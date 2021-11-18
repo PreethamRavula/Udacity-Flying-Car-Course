ccc6fdb4362f1b0ad3e2dfcd5cfb38257d69ce7b
Sri Preetham Sai Ravula <preethamsai.ravula@gmail.com>
2021-11-18 15:04:35 -0800

    Comitted code  file for backyard flyer implemented on a crazyflie drone

---
+++ Project_1_The_Backyard_Flyer/backyard_flyer_updated_crazyflie.py
@@ -0,0 +1,239 @@
+import argparse
+import time
+import math
+import numpy as np
+from enum import Enum
+from udacidrone import Drone
+from udacidrone.connection import CrazyflieConnection # addded for Crazyflie drone implementation
+from udacidrone.connection import MavlinkConnection, WebSocketConnection  # noqa: F401
+from udacidrone.messaging import MsgID
+
+
+class States(Enum):
+    MANUAL = 0
+    ARMING = 1
+    TAKEOFF = 2
+    WAYPOINT = 3
+    LANDING = 4
+    DISARMING = 5
+
+
+class BackyardFlyer(Drone):
+
+    def __init__(self, connection):
+        super().__init__(connection)
+        self.target_position = np.array([0.0, 0.0, 0.0, 0.0])
+        self.all_waypoints = self.calculate_box()
+        self.in_mission = True
+        self.check_state = {}
+
+        # initial state
+        self.flight_state = States.MANUAL
+
+        # TODO: Register all your callbacks here
+        self.register_callback(MsgID.LOCAL_POSITION, self.local_position_callback)
+        self.register_callback(MsgID.LOCAL_VELOCITY, self.velocity_callback)
+        self.register_callback(MsgID.STATE, self.state_callback)
+
+    def local_position_callback(self):
+        """
+        TODO: Implement this method
+
+        This triggers when `MsgID.LOCAL_POSITION` is received and self.local_position contains new data
+        """
+        if self.flight_state == States.MANUAL: # Adding this line for Crazyflie as drone is in Manual state at the start
+            self.takeoff_transition()
+
+        if self.flight_state == States.TAKEOFF:
+
+            #condition for waypoint Transition
+            if -1.0*self.local_position[2] > 0.95*self.target_position[2]:
+                self.all_waypoints = self.calculate_box()
+                self.waypoint_transition()
+
+        elif self.flight_state == States.WAYPOINT:
+
+            #if np.linalg.norm(self.target_position[0:2] - self.local_position[0:2]) < 1.0:
+            if np.linalg.norm(self.target_position[0:2] - self.local_position[0:2]) < 0.2: # new waypoint acceptance threshold for Crazyflie
+
+                if len(self.all_waypoints) > 0:
+                    self.waypoint_transition()
+
+                else:
+                    #if np.linalg.norm(self.local_velocity[0:2]) < 1.0:
+                    if np.linalg.norm(self.local_velocity[0:2]) < 0.25:
+                        self.landing_transition()
+
+
+    def velocity_callback(self):
+        """
+        TODO: Implement this method
+
+        This triggers when `MsgID.LOCAL_VELOCITY` is received and self.local_velocity contains new data
+        """
+        # Code for simulator
+        # if self.flight_state == States.LANDING:
+        #     print("the distance to land is : {}".format(self.global_position[2]-self.global_home[2]))
+        #     if self.global_position[2]-self.global_home[2] < 0.1:
+        #         if abs(self.global_position[2]) < 0.01:
+        #             print("Approaching Home loaction")
+        #             self.disarming_transition()
+
+        # Code for Crazyflie Drone
+        if self.flight_state == States.LANDING:
+            if abs(self.local_position[2] < 0.1):
+                self.manual_transition()
+
+
+    def state_callback(self):
+        """
+        TODO: Implement this method
+
+        This triggers when `MsgID.STATE` is received and self.armed and self.guided contain new data
+        """
+        if not self.in_mission:
+            return
+
+        if self.flight_state == States.MANUAL:
+            self.arming_transition()
+
+        elif self.flight_state == States.ARMING:
+            if self.armed:
+                self.takeoff_transition()
+
+        elif self.flight_state == States.DISARMING:
+            if not self.armed and not self.guided:
+                self.manual_transition()
+
+    def calculate_box(self):
+        """TODO: Fill out this method
+
+        1. Return waypoints to fly a box
+        """
+        # waypoints for simulator code
+        # fly_waypoints = [[20.0, 0.0, 3.0, 0.0],
+        #                  [20.0, 20.0, 3.0, 0.0],
+        #                  [0.0, 20.0, 3.0, 0.0],
+        #                  [0.0, 0.0, 3.0, 0.0]]
+
+        #return fly_waypoints
+
+        # waypoints for Crazyflie drone:
+        current_position = self.local_position
+        current_position[2] = 0
+        local_waypoints = [current_position + [0.5, 0.0, 0.5], current_position + [0.5, 0.5, 0.5], current_position + [0.0, 0.5, 0.5], current_position + [0.0, 0.0, 0.5]]
+
+        return local_waypoints
+
+    def arming_transition(self):
+        """TODO: Fill out this method
+
+        1. Take control of the drone
+        2. Pass an arming command
+        3. Set the home location to current position
+        4. Transition to the ARMING state
+        """
+        print("arming transition")
+        self.take_control()
+        self.arm()
+        self.set_home_position(self.global_position[0],
+                               self.global_position[1],
+                               self.global_position[2])
+        self.flight_state = States.ARMING
+
+    def takeoff_transition(self):
+        """TODO: Fill out this method
+
+        1. Set target_position altitude to 3.0m
+        2. Command a takeoff to 3.0m
+        3. Transition to the TAKEOFF state
+        """
+        print("takeoff transition")
+        target_altitude = 0.5
+        self.target_position[2] = target_altitude
+        self.takeoff(target_altitude)
+        self.flight_state = States.TAKEOFF
+
+    def waypoint_transition(self):
+        """TODO: Fill out this method
+
+        1. Command the next waypoint position
+        2. Transition to WAYPOINT state
+        """
+        print("waypoint transition")
+        """
+        current waypoint is the waypoint drone travels to next,
+        current position of the drone will be assigned to the drone
+        in local position callback function and the point where it takes off is its current Position.
+        """
+        current_waypoint =  self.all_waypoints.pop(0)
+        #assigning the target position
+        self.target_position = np.array(current_waypoint[0:4])
+        print("The next target position is {} ".format(self.target_position))
+        self.cmd_position(*current_waypoint)
+        self.flight_state = States.WAYPOINT
+
+    def landing_transition(self):
+        """TODO: Fill out this method
+
+        1. Command the drone to land
+        2. Transition to the LANDING state
+        """
+        print("landing transition")
+        self.land()
+        # Change state to landing
+        self.flight_state = States.LANDING
+
+    def disarming_transition(self):
+        """TODO: Fill out this method
+
+        1. Command the drone to disarm
+        2. Transition to the DISARMING state
+        """
+        print("disarm transition")
+        self.disarm()
+        #transition to disarm state
+        self.flight_state = States.DISARMING
+
+    def manual_transition(self):
+        """This method is provided
+
+        1. Release control of the drone
+        2. Stop the connection (and telemetry log)
+        3. End the mission
+        4. Transition to the MANUAL state
+        """
+        print("manual transition")
+
+        self.release_control()
+        self.stop()
+        self.in_mission = False
+        self.flight_state = States.MANUAL
+
+    def start(self):
+        """This method is provided
+
+        1. Open a log file
+        2. Start the drone connection
+        3. Close the log file
+        """
+        print("Creating log file")
+        self.start_log("Logs", "NavLog.txt")
+        print("starting connection")
+        self.connection.start()
+        print("Closing log file")
+        self.stop_log()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', type=int, default=5760, help='Port number')
+    parser.add_argument('--host', type=str, default='127.0.0.1', help="host address, i.e. '127.0.0.1'")
+    args = parser.parse_args()
+
+    #conn = MavlinkConnection('tcp:{0}:{1}'.format(args.host, args.port), threaded=False, PX4=False)
+    #conn = WebSocketConnection('ws://{0}:{1}'.format(args.host, args.port))
+    conn = CrazyflieConnection('radio://0/80/2M') #added new connection object for crazyflie drone radio://interface id/interface channel/interface speed
+    drone = BackyardFlyer(conn)
+    time.sleep(2)
+    drone.start()
